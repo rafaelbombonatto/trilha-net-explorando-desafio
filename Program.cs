2601e6fe8c45b24407a9426fee4f3e2cd71902ae
Rafael Bombonatto Martins Fernandes <rafael.bombonatto@gmail.com>
2022-10-04 09:49:39 -0300

    Solução do projeto de hotelaria

--- Program.cs
+++ Program.cs
@@ -8,12 +8,14 @@ List<Pessoa> hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
 Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p3 = new Pessoa(nome: "Hóspede 3");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+hospedes.Add(p3);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 5, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
 Reserva reserva = new Reserva(diasReservados: 5);
@@ -22,4 +24,4 @@ reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria().ToString("C")}");
